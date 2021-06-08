4488a1fa8fc114e7693edba16947642291d7d983
Iliianotapoet <ilya.10189@gmail.com>
2021-06-08 18:17:21 +0300

    add object panel v0.0.1 & tools panel v0.0.3

--- src/main_java/controllers/main_window/MainWindowController.java
+++ src/main_java/controllers/main_window/MainWindowController.java
@@ -1,24 +1,21 @@
 package main_java.controllers.main_window;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import main_java.controllers.canvas.CanvasController;
 
 public class MainWindowController extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        CustomPanelController pane = new CustomPanelController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/view/main_window/MainWindowView.fxml"));
+        Parent root = loader.load();
 
-        // pane.getChildren().add(new CanvasController());
-
-        Scene scene = new Scene(pane, 800, 600);
+        Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
+        primaryStage.setTitle("VecLab");
         primaryStage.show();
     }
 
--- src/main_java/controllers/main_window/tools_panel/ToolsPanelController.java
+++ src/main_java/controllers/main_window/tools_panel/ToolsPanelController.java
@@ -1,6 +1,22 @@
 package main_java.controllers.main_window.tools_panel;
 
-import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.FlowPane;
 
-public class ToolsPanelController extends GridPane {
+import java.io.IOException;
+
+public class ToolsPanelController extends FlowPane {
+    public ToolsPanelController() {
+        super();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                "/resources/view/main_window/tools_panel/ToolsPanelView.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 }
