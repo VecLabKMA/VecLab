0b0f51e8aa6a60dc2b0e46d5b0582588ce6e394a
Iliianotapoet <ilya.10189@gmail.com>
2021-06-15 23:37:46 +0300

    delete empty classes

--- src/main_java/models/main_window/tools_panel/tools_buttons/ToolButtonBaseModel.java
+++
@@ -1,6 +0,0 @@
-package main_java.models.main_window.tools_panel.tools_buttons;
-
-// base class
-public abstract class ToolButtonBaseModel {
-    
-}
--- src/main_java/models/shapes/EllipseModel.java
+++
@@ -1,4 +0,0 @@
-package main_java.models.shapes;
-
-public class EllipseModel extends ShapeBaseModel {
-}
